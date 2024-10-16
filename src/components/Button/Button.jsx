e4fa779c9d1f825e93780fe5c0d9a7675fe6a6c6
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-10-17 00:28:16 +0200

    Remove focus from button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,17 @@
-import css from "./Button.module.css"
+import css from './Button.module.css'
 
-const Button = ({ styleType, children, onClick }) => (
-   <button className={css[styleType]} onClick={onClick}>
-      {children}
-   </button>
-)
+const Button = ({ styleType, children, onClick }) => {
+   const handleClick = e => {
+      e.preventDefault()
+      e.target.blur()
+      onClick()
+   }
+
+   return (
+      <button className={css[styleType]} onClick={handleClick}>
+         {children}
+      </button>
+   )
+}
 
 export default Button
